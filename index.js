0d70dea0e97e9c3039b4481cc179a5c537a9d951
unknown <bernardo.caiano@outlook.pt>
2018-03-09 11:47:48 +0000

    resto da ficha 1

--- index.js
+++ index.js
@@ -32,5 +32,36 @@ function funcD() {
 function funcE() {
     let peso = prompt("Indique o seu peso em kg:")
     let altura = prompt("Indique a sua altura em m:")
-    let imc = peso / altura
+    let imc = peso / (altura * altura)
+    if (imc < 18.5){
+        console.log("Abaixo do peso")
+    }
+    else if (imc >= 18.5 & imc < 24.9){
+        console.log("Peso normal")
+    }
+    else if (imc >= 24.9 & imc < 29.9){
+        console.log("Sobrepeso")
+    }
+    else if (imc >= 29.9 & imc < 34.9){
+        console.log("Obesidade grau I")
+    }
+    else if (imc >= 34.9 & imc < 39.9){
+        console.log("Obesidade grau II")
+    }
+    else if (imc >= 39.9){
+        console.log("Obesidade grau III ou mórbida")
+    }
+}
+//Alinea F
+function funcF() {
+    let S = prompt("Escreva uma palavra:")
+    let N = prompt("Digite um número:")
+    for (let i = 0; i < N; i++)
+    {
+        console.log(S)
+    }
+}
+//Alinea G
+function funcG() {
+    
 }
