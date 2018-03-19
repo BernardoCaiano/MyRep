1370f86e9d6fdeb7ec3f9c7d8620bb0c0130b823
unknown <bernardo.caiano@outlook.pt>
2018-03-19 13:17:47 +0000

    continuaçao da resoluçao da ficha 4

--- POO/ficha 4/index.js
+++ POO/ficha 4/index.js
@@ -11,7 +11,7 @@ function funcA() {
     let estudante = {
         nome: "David Silva",
         curso: "POO",
-        ano: 12
+        nota: 12
     }
     //funçao alinea i)
     function propriedades() {
@@ -29,9 +29,9 @@ function funcA() {
     } 
     //funçao alinea ii)
     function remover() {
-        show("ii) -> antes: " + estudante.ano)
-        delete estudante.ano
-        show("       depois: " + estudante.ano)
+        show("ii) -> antes: " + estudante.nota)
+        delete estudante.nota
+        show("       depois: " + estudante.nota)
     }
     function tamanho() {
         for (let i in estudante){
@@ -46,3 +46,73 @@ function funcA() {
     //resoluçao alinia iii)
     tamanho()
 }
+//Alinea B
+function funcB() {
+    let notas = [
+        {
+        nome: 'Rui',
+        nota: 8
+        },
+        {
+        nome: 'Ana',
+        nota: 12
+        },
+        {
+        nome: 'Carla',
+        nota: 14
+        }
+    ] 
+    //alinea i)
+    let nome = prompt("Indique o nome:")
+    let nota = parseInt(prompt("Indique a nota:"))
+    let teste = {nome, nota}
+    notas.push(teste)
+    //alinea ii)
+    function media() {
+        let soma = 0
+        let media = 0
+        let cont = 0
+        for (let i = 0; i < notas.length; i++){
+           soma += notas[i].nota
+           cont++
+        }
+        media = soma / cont
+        show("média: " + media)
+    }
+    //alinea iii)
+    function positivas() {
+        let cont2 = 0
+        let resposta = ""
+        for (let i = 0; i < notas.length; i++){
+            if (notas[i].nota >= 10){
+                if (cont2 > 0){
+                    resposta += ", " + notas[i].nome 
+                    
+                } 
+                else {
+                    resposta += notas[i].nome
+                }
+                cont2++
+            }
+        }
+        show("Notas positivas a POO: " + resposta)
+    }
+    media()
+    positivas()
+}    
+//Alinea C
+function funcC() {
+    function Carro(marca, matricula, cor, depositoAtual, tipoCombustivel )  {
+        this.marca = marca
+        this.matricula = matricula
+        this.cor = cor
+        this.depositoAtual = depositoAtual
+        this.tipoCombustivel = tipoCombustivel
+    }
+    let carro1 = new Carro("Ford", "91-GH-15", "verde", 40, "Gasóleo")
+    let carro2 = new Carro("Opel", "23-AB-23", "branco", 50, "Gasolina")
+    show(carro1.cor)
+    carro1.cor = prompt("Qual é a cor do carro:")
+    show(carro1.cor)
+}
+    
